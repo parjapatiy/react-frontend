f9ca3782a8d5d2dfeb7fa297a33382fe837bca6a
parjapatiy <yanshulp@gmail.com>
2021-06-25 12:41:16 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://john.squareops.xyz:3000/api';
+const API_ROOT = 'https://john.squareops.xyz/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
